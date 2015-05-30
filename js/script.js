4bd78789816fdba9791374fe2f56f1c6f2d4e292
Christoph Häckel <ch@famobi.com>
2015-05-30 21:54:05 +0200

    famobi API integration

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-!function($) {
+/* jshint undef: true, unused: true */
+/* global famobi, faZepto */
+(function($) {
 	'use strict';
 
 	var game = {};
@@ -7,27 +9,55 @@
 	game.step = 50;
 
 	game.list = $('.list');
+	game.finished = 0;
+
+	game.lastTime = +(new Date());
+	game.idle = 0;
+	game.deltaTime = 0;
+
+	// fired on every tick, when a frame is requested
+	game.tick = function() {
+		var now = +(new Date());
+
+		game.deltaTime = now - game.lastTime;
+		game.lastTime = now;
+
+		trackDistance();
+
+		if (game.dist >= 1000) {
+			if (!game.finished) {
+				finishGame();
+			}
+		}
+	};
+
+	// ticker
+	game.update = function() {
+		this.tick();
+
+		window.requestAnimationFrame(this.update);
+	};
 
 	// position the lines from the top of the screen according to their data-meters attribute
-	var posLines = function() {
+	function posLines() {
 		var $elem = $('.meters');
 
-		$elem.each(function(value, key) {
+		$elem.each(function() {
 			var $this = $(this),
 				m = parseFloat($this.attr('data-meters'));
 
 			$this.css('top', m*1000 + 'px');
 		});
-	};
+	}
 
-	// fill lines in 50m steps for better orientation
-	var fillLines = function() {
+	// fill lines in steps for better orientation
+	function fillLines() {
 		var $elem = $('.meters'),
 			goal = game.goal,
 			stepWidth = game.step,
 			steps = {};
 
-		$elem.each(function(value, key) {
+		$elem.each(function() {
 			var $this = $(this),
 				m = parseFloat($this.attr('data-meters'));
 
@@ -47,34 +77,49 @@
 			}
 		}
 		game.list.append(li);
-	};
+	}
 
-	game.finished = 0;
 	game.dist = 0;
 	game.offset = document.querySelector('.list').offsetTop;
+	game.lastDistance = 0;
+	game.deltaDistance = 0;
 	function trackDistance() {
 		game.dist = (window.scrollY - game.offset)/1000;
 		document.querySelector('#m').innerHTML = numFormat(Math.max(0,game.dist));
 
-		if (game.dist >= 1000) {
-			if (!game.finished)
-				finishGame();
+		// check if distance has change in the last X sec
+		if (game.dist - game.lastDistance) {
+			game.idle = 0;
+		} else {
+			game.idle = 1;
+
+			if (game.deltaTime > 1E3) {
+				console.log(game.dist - game.lastDistance, 'game.dist - game.lastDistance');
+				console.log(game.deltaTime, 'delta > 1 sec');
+
+				submitHighscore();
+			}
 		}
-		window.requestAnimationFrame(trackDistance);
-	};
+
+		game.lastDistance = game.dist;
+	}
 
 	function numFormat(e){
-		return e.toFixed(3).replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, '$1,');
-	};
+		return e.toFixed(3).replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, '$1.');
+	}
 
 	function finishGame() {
 		game.finished = 1;
-		alert("You did it! Respect :)");
+		alert("You did it! Respect 8)");
 		document.querySelector('.finish').style.display = 'block';
-		document.querySelector('.finish').onclick = function() {
+		document.querySelector('.finish').onclick = submitHighscore;
+		document.querySelector('body').style.minHeight = '1000000000000000px';
+	}
+
+	function submitHighscore() {
+		requestAnimationFrame(function() {
 			famobi.submitHighscore(0, game.dist);
-		};
-		document.querySelector('.game').style.height = '1000000000000000px';
+		});
 	}
 
 	game.start = function() {
@@ -84,4 +129,4 @@
 	};
 
 	game.start();
-}(Zepto);
+})(faZepto);
